5427bba97405a74afc167e1a1adecef1f2eb61ea
GIKroch <45598766+GIKroch@users.noreply.github.com>
2020-12-07 13:39:45 +0100

    additional improvements app description

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ Welcome to my Python powered web application designed for technical analysis of 
 
 When I graduated from my Master's in Data Science in July 2020, I instantly started thinking on how to further develop my analytical skills. I'm learning a lot on daily basis working as a data analyst at Citi. However, there are so many things one can learn, that it would be a huge loss for me not to explore other opportunities. 
                             
-To grow in the analytical area one needs to get some data to practice at. Hence, the first question I had to ask myself before starting the project was: “Where to look for easily accessible and interesting data?”. The answer was straightforward – US stock market. Why was it so obvious for me? Well, I’ve been interested in the big finance world since childhood but never tried investing. I felt it was the right time to get into a trading path. To be honest, I got so excited about this idea that I couldn’t really resist myself from doing my first transaction. I bought 1 Apple stock through my Polish broker account (7$ fee for each foreign investment!!!) and quickly lost tens of bucks. After I closed the position it became clear that impulsive investing is not the best way to make money on a stock market. I switched to a more reasonable strategy. First, build a technical analysis application, then start making deals based on it. 
+To grow in the analytical area one needs to get some data to practice at. Hence, the first question I had to ask myself before starting the project was: “Where to look for easily accessible and interesting data?”. The answer was straightforward – US stock market. Why was it so obvious for me? Well, I’ve been interested in the big finance world since childhood but never tried investing. I felt it was the right time to get into a trading path. To be honest, I got so excited about this idea that I couldn’t really resist myself from doing my first transaction. I bought 1 Apple stock through my Polish brokerage account (7$ fee for each foreign investment!!!) and quickly lost tens of bucks. After I closed the position it became clear that impulsive investing is not the best way to make money on a stock market. I switched to a more reasonable strategy. First, build a technical analysis application, then start making deals based on it. 
 
 ## Data Sources
 This app has been created for trading on eToro platform. To keep things simple I've focused only on NYSE and NASDAQ equities. The data utilized comes from multiple sources, many of which were parsed with web scraping techniques. Please find the list of sources of specific data attributes below (link to a scraping script attached, if applicable):
@@ -228,7 +228,7 @@ This app has been created for trading on eToro platform. To keep things simple I
 * Machine Learning for prediction of prices. 
 
 ## How does it work? 
-The app presented here, on Heroku, is based on a snapshot of database (3rd of December 2020). Due to size-limitation, the data only covers records from 1st of January 2018. The app code repository is available at [GitHub](https://github.com/GIKroch/Stocks). The repository is bonded with Heroku. 
+The app presented here, on Heroku, is based on a snapshot of a database (3rd of December 2020). Due to a size-limitation, the data only covers records from 1st of January 2018. The app's code repository is available at [GitHub](https://github.com/GIKroch/Stocks). The repository is bonded with Heroku. 
 
 At my private desktop the app's database is updated daily during a working week. The update process is handled by my Raspberry Pi 4, which serves also as a network drive. Therefore, when the app is run from my desktop it can always access the most recent data. 
 
@@ -237,7 +237,7 @@ The app has been created in Python, the packages used:
 * Backend:
     * Dash – Web interface
     * Plotly – Plotting interface
-    * Sqlite3 – Database
+    * Sqlite3 – Database -> Planned migration to Postgresql
 * Computations: 
     * Pandas 
     * Numpy 
